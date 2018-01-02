af945c3277c4114af3f2619e222a7f0565c8c503
Dan Mateer <dan@pullrequest.com>
2018-01-02 13:14:14 -0600

    Added changes to break

--- sample_code.py
+++ sample_code.py
@@ -1,7 +1,7 @@
 n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
-# This flattens the list 
-def flatten(lists):
-  results = []
+# This flattens the list
+def flatten(lists)):
+  results = [""]
   for items in lists:
     for numbers in items:
       results.append(numbers)
