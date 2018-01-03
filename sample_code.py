d5b4a1703503206638a30512b886910fd631f7ca
Dan Mateer <dan@pullrequest.com>
2018-01-02 18:29:58 -0600

    Added FIXME

--- sample_code.py
+++ sample_code.py
@@ -1,12 +1,12 @@
 n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
 # This flattens the list
 def flatten(lists):
-  results = ['']
+  results = []
   for items in lists:
     for numbers in items:
       results.append(numbers)
-  return results
-# todo: more stuff
+   return results
+# FIXME: more stuff
 
 
 print flatten(n)
