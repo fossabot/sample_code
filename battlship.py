8e161c9aa249f8b37707ba476074b15fc4e831c7
dnmtr <34169713+danMateer@users.noreply.github.com>
2019-04-24 17:24:40 -0500

    Update battlship.py

--- battlship.py
+++ battlship.py
@@ -4,10 +4,6 @@ def print_board(board):
   for row in board:
     print " ".join(row)
 
-# Added line
-# Added another line
-# Added a third line
-
 print_board(board)
 
 def random_row(board):
@@ -20,12 +16,6 @@ ship_row = random_row(board)
 ship_col = random_col(board)
 print ship_row
 print ship_col
-
-#This gives the user 4 tries to sink the battleship
-for turn in range(4):
-  print "Turn", turn + 1
-  guess_row = int(raw_input("Enter latitude (1-4): "))
-  guess_col = int(raw_input("Enter longitude (1-4): "))
 
 # This handles the outcome of each guess
   if guess_row == ship_row and guess_col == ship_col:
