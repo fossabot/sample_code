22036e0fad28b975b6282cd01030ab7ba03b4e9f
Dan Mateer <dan@pullrequest.com>
2019-05-08 14:47:20 -0500

    big branch2

--- refactor_practice.rb
+++
@@ -1,9 +0,0 @@
-require 'prime'
-
-def first_n_primes(n)
-  return "n must be an integer." unless n.is_a? Integer
-  return "n must be greater than 0." unless n > 0
-  Prime.first n
-end
-
-first_n_primes(10)
