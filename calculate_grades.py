bb5b7e2d58b08a7f9a0defa84c89dc0cd74ce6f7
dnmtr <34169713+danMateer@users.noreply.github.com>
2019-08-05 09:54:07 -0500

    Update calculate_grades.py

--- calculate_grades.py
+++ calculate_grades.py
@@ -2,6 +2,7 @@ grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 53.5]
 
 
 def print_grades(grades_input):
+  """TODO: post grades to endpoint"""
   for grade in grades_input:
     print grade
 
@@ -14,6 +15,7 @@ def grades_sum(scores):
 
 
 def grades_average(grades_input):
+  """FIXME: limit float to 2"""
   sum_of_grades = grades_sum(grades_input)
   average = sum_of_grades / float(len(grades_input))
   return average
